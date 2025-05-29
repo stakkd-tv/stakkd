c0131cf860370134eae4e84d838db85a34b05448
Diogo <diogo6204@gmail.com>
2025-05-29 18:32:03 +0100

    ui improvements

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,10 +3,16 @@ Country.delete_all
 Language.delete_all
 Genre.delete_all
 Movie.delete_all
+Session.delete_all
 User.delete_all
+Company.delete_all
 
 puts "=== Creating users ==="
 User.create(username: "testies", email_address: "test@example.com", password: "123456")
+
+
+puts "=== Creating companies ==="
+Company.create(name: "testworks", homepage: "https://testworks.com", description: "A small company founded in the Poortuguese city of Sugoma.")
 
 puts "=== Importing genres ==="
 Sync::Genres.new.start
