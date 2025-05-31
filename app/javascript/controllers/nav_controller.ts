e3754e241f520f64babf7f1109af21e44ee27b42
Tanveer Najib <tanveernajib@gmail.com>
2025-05-31 16:07:53 +0100

    Add mobile navigation

--- app/javascript/controllers/nav_controller.ts
+++ app/javascript/controllers/nav_controller.ts
@@ -2,11 +2,13 @@ import { Controller } from '@hotwired/stimulus'
 
 // Connects to data-controller="nav"
 export default class extends Controller {
-  static targets = ['search', 'searchWrapper', 'button']
+  static targets = ['search', 'searchWrapper', 'button', 'user']
 
   declare readonly searchTarget: HTMLElement
   declare readonly searchWrapperTarget: HTMLElement
+  declare readonly userTarget: HTMLElement
   declare readonly buttonTargets: HTMLElement[]
+  declare readonly hasUserTarget: boolean
 
   connect () {
     this.searchWrapperTarget.addEventListener('click', () => {
@@ -24,5 +26,20 @@ export default class extends Controller {
         btn.classList.remove('!hidden')
       })
     })
+
+    const sidebar = document.getElementById('sidebar')
+    if (this.hasUserTarget && sidebar) {
+      const closeSidebar = document.getElementById('close-sidebar') as HTMLElement
+
+      closeSidebar.addEventListener('click', () => {
+        sidebar.classList.add('hidden')
+        sidebar.classList.remove('flex')
+      })
+
+      this.userTarget.addEventListener('click', () => {
+        sidebar.classList.add('flex')
+        sidebar.classList.remove('hidden')
+      })
+    }
   }
 }
