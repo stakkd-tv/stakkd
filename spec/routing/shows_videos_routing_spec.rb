4de217b0f15d26bd87eac8056f6b3e9cab534216
Tanveer Najib <tanveernajib@gmail.com>
2025-09-07 11:19:45 +0100

    Fix routing spec

--- spec/routing/shows_videos_routing_spec.rb
+++ spec/routing/shows_videos_routing_spec.rb
@@ -3,15 +3,15 @@ require "rails_helper"
 RSpec.describe VideosController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(get: "/show/1/videos").to route_to("videos#index", movie_id: "1")
+      expect(get: "/shows/1/videos").to route_to("videos#index", show_id: "1")
     end
 
     it "routes to #create" do
-      expect(post: "/show/1/videos").to route_to("videos#create", movie_id: "1")
+      expect(post: "/shows/1/videos").to route_to("videos#create", show_id: "1")
     end
 
     it "routes to #destroy" do
-      expect(delete: "/show/1/videos/1").to route_to("videos#destroy", movie_id: "1", id: "1")
+      expect(delete: "/shows/1/videos/1").to route_to("videos#destroy", show_id: "1", id: "1")
     end
   end
 end
