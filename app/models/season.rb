05c16bb63c87555468bbb4eda95ebcee4bcc71e9
Tanveer Najib <tanveernajib@gmail.com>
2026-02-24 15:07:34 +0000

    Validate numericality of season number

--- app/models/season.rb
+++ app/models/season.rb
@@ -5,4 +5,5 @@ class Season < ApplicationRecord
   # Validations
   validates :translated_name, :original_name, presence: true
   validates :number, uniqueness: {scope: [:show_id]}
+  validates :number, numericality: {greater_than_or_equal_to: 0}
 end
--- spec/models/season_spec.rb
+++ spec/models/season_spec.rb
@@ -10,5 +10,6 @@ RSpec.describe Season, type: :model do
     it { should validate_presence_of(:translated_name) }
     it { should validate_presence_of(:original_name) }
     it { should validate_uniqueness_of(:number).scoped_to([:show_id]) }
+    it { should validate_numericality_of(:number).is_greater_than_or_equal_to(0) }
   end
 end
