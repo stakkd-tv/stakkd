32a0a79d7de27597e567bb1fa836653a5da6bb30
Tanveer Najib <tanveernajib@gmail.com>
2026-03-03 14:25:55 +0000

    Add missing spec for seasons edit

--- spec/views/seasons/edit.html.erb_spec.rb
+++ spec/views/seasons/edit.html.erb_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "seasons/edit", type: :view do
-  let(:season) { FactoryBot.create(:season) }
+  let(:season) { FactoryBot.create(:season, number: 1) }
 
   before(:each) do
     assign(:season, season)
@@ -16,6 +16,15 @@ RSpec.describe "seasons/edit", type: :view do
       assert_select "input[name='season[translated_name]']"
       assert_select "input[name='season[original_name]']"
       assert_select "textarea[name='season[overview]']"
+    end
+  end
+
+  context "when the season is a special" do
+    let(:season) { FactoryBot.create(:season, number: 1).show.ordered_seasons.first }
+
+    it "renders a disabled input for the number field" do
+      render
+      assert_select "input[name='season[number]'][disabled]"
     end
   end
 end
