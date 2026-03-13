dbac865aa0eb1beed2189b70abe9953813145a8f
Tanveer Najib <tanveernajib@gmail.com>
2026-03-13 09:49:30 +0000

    WIP nicer error messages

--- config/initializers/field_with_errors.rb
+++ config/initializers/field_with_errors.rb
@@ -3,7 +3,14 @@ ActionView::Base.field_error_proc = proc do |html_tag, _instance|
   element = fragment.children.first
 
   if element
-    element.set_attribute("data-error", "true")
+    element.set_attribute("data-field-error", "true")
+    if element.name != "label"
+      new_node = Nokogiri::XML::Node.new("span", fragment)
+      new_node.content = "Field #{_instance.error_message.to_sentence}"
+      new_node.set_attribute("class", "mt-2 inline-block")
+      element.add_next_sibling(new_node)
+    end
+
     fragment.to_html.html_safe
   else
     html_tag.html_safe
