b06303eccf2fcd2d633010f80fae80e1e07f925b
Tanveer Najib <tanveernajib@gmail.com>
2026-07-17 16:58:39 +0100

    Add load more functionality to search page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,10 @@
 class SearchController < ApplicationController
   TABS = {
-    movies: {klass: Movie, query_by: "translated_title,original_title,alternative_names", collection: "Movie", image_type: :poster, fallback: "2:3.png"},
-    shows: {klass: Show, query_by: "translated_title,original_title,alternative_names", collection: "Show", image_type: :poster, fallback: "2:3.png"},
-    people: {klass: Person, query_by: "original_name,translated_name,aka", collection: "Person", image_type: :image, fallback: "2:3.png"},
-    companies: {klass: Company, query_by: "name", collection: "Company", image_type: :logo, fallback: "1:1.png", aspect: "aspect-square"},
-    users: {klass: User, query_by: "username", collection: "User", image_type: :avatar, fallback: "user.png", aspect: "aspect-square"}
+    movies: {klass: Movie, query_by: "translated_title,original_title,alternative_names", collection: "Movie", image_type: :poster},
+    shows: {klass: Show, query_by: "translated_title,original_title,alternative_names", collection: "Show", image_type: :poster},
+    people: {klass: Person, query_by: "original_name,translated_name,aka", collection: "Person", image_type: :image},
+    companies: {klass: Company, query_by: "name", collection: "Company", image_type: :logo, aspect: "aspect-square"},
+    users: {klass: User, query_by: "username", collection: "User", image_type: :avatar, aspect: "aspect-square"}
   }
 
   def index
@@ -20,24 +20,54 @@ class SearchController < ApplicationController
       klass = TABS[tab][:klass]
       initial_results = klass.where(id: result_ids).map {
         image_type = TABS[tab][:image_type]
-        args = image_type == :avatar ? {use_fallback: false} : {variant: :medium, use_fallback: false}
-        image = image_type ? it.send(image_type, **args) : nil
-        {poster_path: image ? url_for(image) : nil, title: it.to_s, slug: it.slug, aspect: TABS[tab][:aspect]}
+        args = image_type == :avatar ? {} : {variant: :medium}
+        image = it.send(image_type, **args)
+        {image:, title: it.to_s, slug: it.slug, aspect: TABS[tab][:aspect]}
       }
       {
         tab: tab.to_s,
         initial_results:,
-        count: result["found"] || 0,
-        fallback: TABS[tab][:fallback]
+        count: result["found"] || 0
       }
     end.sort_by { |hash| -hash[:count] }
   end
 
   def show
-    # TODO: Load more
+    @tab = params[:id].to_sym
+    tab_info = TABS[@tab]
+    return render(status: :not_found) unless tab_info
+
+    @page = (params[:page] || 1).to_i
+    search_results = execute_single_search(tab_info, @page)
+    @results = search_results.map {
+      image_type = tab_info[:image_type]
+      args = image_type == :avatar ? {} : {variant: :medium}
+      image = it.send(image_type, **args)
+      {image:, title: it.to_s, slug: it.slug, aspect: tab_info[:aspect]}
+    }
+    @has_more = search_results.next_page.present?
+
+    respond_to do |format|
+      format.turbo_stream
+    end
   end
 
   private
+
+  def execute_single_search(tab, page)
+    klass = tab[:klass]
+    klass.search(
+      params[:q],
+      tab[:query_by],
+      {
+        page: page,
+        per_page: ApplicationController::GLOBAL_PER_PAGE
+      }
+    )
+  rescue
+    # TODO: Log failure
+    []
+  end
 
   def execute_multi_search
     searches = TABS.values.map { {collection: "#{it[:collection]}_#{Rails.env}", query_by: it[:query_by]} }
--- app/javascript/controllers/search_results_controller.ts
+++ app/javascript/controllers/search_results_controller.ts
@@ -1,18 +1,11 @@
 import { Controller } from '@hotwired/stimulus'
 
-type Result = {
-  poster_path: string,
-  title: string,
-  slug: string,
-  aspect: string | null
-}
-
 // Connects to data-controller="search-results"
 export default class extends Controller {
-  static targets = ['tab', 'resultsContainer']
+  static targets = ['tab', 'resultsGrid']
 
   declare readonly tabTargets: HTMLElement[]
-  declare readonly resultsContainerTarget: HTMLElement
+  declare readonly resultsGridTargets: HTMLElement[]
 
   declare currentTab: HTMLElement
 
@@ -32,29 +25,19 @@ export default class extends Controller {
     }
     this.currentTab = newTab
     this.currentTab.setAttribute('data-sidetab-active', 'true')
-    this.updateResults()
+    this.toggleGridVisibility()
   }
 
-  updateResults () {
-    this.resultsContainerTarget.innerHTML = ''
-    const rawResults = this.currentTab.getAttribute('data-initial-results')
-    if (!rawResults) return
+  toggleGridVisibility () {
+    const activeTabName = this.currentTab.getAttribute('data-tab')
+    if (!activeTabName) return
 
-    const tab = this.currentTab.getAttribute('data-tab')
-    const results: Result[] = JSON.parse(rawResults)
-    results.forEach(result => {
-      const fallback = this.currentTab.getAttribute('data-fallback-src')
-      const html = `
-        <a href="/${tab}/${result.slug}" class="h-fit border border-pop rounded-lg overflow-hidden relative" data-turbo="false">
-          <img src="${result.poster_path ?? fallback}" class="w-full ${result.aspect ?? 'aspect-2/3'} object-cover">
-          <div class="absolute w-full p-2 sm:p-4 bottom-0 left-0 bg-background/75 border-t border-pop">
-            <div class="flex flex-col gap-2">
-              <h3 class="text-sm sm:text-lg font-bold">${result.title}</h3>
-            </div>
-          </div>
-        </a>
-      `
-      this.resultsContainerTarget.innerHTML += html
+    this.resultsGridTargets.forEach(grid => {
+      if (grid.id === `${activeTabName}-results-container`) {
+        grid.classList.remove('hidden')
+      } else {
+        grid.classList.add('hidden')
+      }
     })
   }
 }
